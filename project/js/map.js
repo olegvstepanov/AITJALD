a2cac53d99af222c4442316594afc43ba164a519
Christoph Kisfeld <spam@b-web.org>
2016-01-28 00:08:33 +0100

    Added a sidebar

--- project/js/map.js
+++ project/js/map.js
@@ -22,7 +22,7 @@ var defaults = {
 L.tileLayer('http://tile.stamen.com/toner-lite/{z}/{x}/{y}.png', {
     attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
 }).addTo(map);
-
+map.invalidateSize();
 
 var featureGroup = L.featureGroup().addTo(map);
 
